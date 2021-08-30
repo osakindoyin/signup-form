eb1ae769e4e381fd5412384813da04d2359190f7
woleakindoyin <akindoyinoluwole@gmail.com>
2021-08-30 07:59:12 +0100

    made changes to all the files

--- signup-form.js
+++ signup-form.js
@@ -1,26 +1,52 @@
-let firstName = document.getElementById('firstName');
-let lastName = "";
-let email = "";
-let password = "";
-
-document.querySelector('.btn').addEventListener("click", function(event){
-    buttonClick(event.target.innerText);
-})
-
-function buttonClick(value){
-    console.log(value != null);
-    if (value != null) {
-        validateForm();
-    } else {
-        return
-    }
-}
-
+/*
 function validateForm() {
-    console.log("now here")
-    if (firstName === "") {  
-        alert("Please provide your name");
-        console.log('here')
+    let firstName = document.forms["userForm"]["firstName"].value;
+    console.log(firstName);
+    if(firstName == ""){
+        alert("Please enter first name");
         return false;
     }
+}
+*/
+
+const submit = document.getElementById("submit");
+
+submit.addEventListener("click", validate);
+
+function validate(e) {
+  e.preventDefault();
+
+  const firstNameField = document.getElementById("firstName");
+  const lastNameField = document.getElementById("lastName");
+  const emailField = document.getElementById("email");
+  const passwordField = document.getElementById("password");
+
+  if (firstNameField.value === "") {
+    const firstNameError = document.getElementById("nameError");
+    firstNameError.classList.add("visible");
+    firstNameField.classList.add("invalid");
+    firstNameError.setAttribute("aria-hidden", false);
+  }
+
+  if (lastNameField.value === "") {
+    const lastNameError = document.getElementById("nameError");
+    lastNameError.classList.add("visible");
+    lastNameField.classList.add("invalid");
+    lastNameError.setAttribute("aria-hidden", false);
+  }
+
+  if (emailField.value === "") {
+    const emailError = document.getElementById("nameError");
+    emailError.classList.add("visible");
+    emailField.classList.add("invalid");
+    emailError.setAttribute("aria-hidden", false);
+  }
+
+  if (passwordField.value === "") {
+    const passwordError = document.getElementById("nameError");
+    passwordError.classList.add("visible");
+    passwordField.classList.add("invalid");
+    passwordError.setAttribute("aria-hidden", false);
+  }
+  return true;
 }
